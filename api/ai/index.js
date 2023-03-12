248005ff86afdd0c2af5b451d7e8f57c85c47966
Abhishek Prakash2 <abhishek.prakash2@bajajfinserv.in>
2023-03-12 12:34:59 +0530

    Fix bug

--- api/ai/index.js
+++ api/ai/index.js
@@ -1,7 +1,7 @@
 import { respond } from "../../src/ai.js";
 import { verify } from "../../src/whatsapp.js";
 
-function createAi(request, response) {
+async function createAi(request, response) {
   if (request.method === "GET") {
     const { status, data } = verify(
       request.query["hub.mode"],
@@ -13,7 +13,7 @@ function createAi(request, response) {
 
     return response.status(status);
   } else if (request.method === "POST" && request.body.entry) {
-    const { status } = respond(request.body.entry[0]);
+    const { status } = await respond(request.body.entry[0]);
 
     return response.status(status);
   }
